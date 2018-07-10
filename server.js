bc7ed9313b2d313d0c2341f8111da61ecd984dd2
Evan <briolate@gmail.com>
2018-07-09 23:35:55 -0400

    Connected mongoose to database

--- server.js
+++ server.js
@@ -1,9 +1,20 @@
-const express = require("express");
-const mongoose = require("mongoose");
+const express = require('express');
+const mongoose = require('mongoose');
+
+const users = require('');
 
 const app = express();
 
-app.get("/", (req, res) => res.send("Hello World"));
+// DB Config
+const db = require('./config/keys').mongoURI;
+
+// Connecto to MongoDB
+mongoose
+  .connect(db)
+  .then(() => console.log('MongoDB Connected'))
+  .catch(err => console.log('err'));
+
+app.get('/', (req, res) => res.send('Hello World'));
 
 const port = process.env.POR || 5000;
 
