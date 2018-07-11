e2ac91063f17033c4c98f74b2453394d03de3b7c
Evan Briolat <briolate@gmail.com>
2018-07-11 18:14:08 -0400

    Creating the JWT

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-  mongoURI: 'mongodb://briolate:Sparty88Dawg.@ds059804.mlab.com:59804/evansdb'
+  mongoURI: 'mongodb://briolate:Sparty88Dawg.@ds059804.mlab.com:59804/evansdb',
+  secretOrKey: 'secret'
 };
