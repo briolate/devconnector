1bae57fd1cee1ecf499c6bf090a6f091815e21db
Evan <briolate@gmail.com>
2018-08-07 13:15:19 -0400

    Secured keys, set up Heroku, set static folder to serve assets if in production, wrote script for post build and deployment

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
-module.exports = {
-  mongoURI: 'mongodb://briolate:Sparty88Dawg.@ds059804.mlab.com:59804/evansdb',
-  secretOrKey: 'secret'
-};
+if (process.env.NODE_ENV === 'production') {
+  module.exports = require('./keys_prod');
+} else {
+  module.exports = require('./keys_dev');
+}
